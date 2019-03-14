e041e37aa85751ccb6163645c0fc166e6816cea2
BattleTank <duggaltanvir@gmail.com>
2019-03-13 17:53:50 -0700

    Alpha Built

---
+++ Waves/Assets/Assests/Scripts/FireBullets.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullets : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float speed = 4.0f;
+
+    public GameObject rocketPrefab;
+    public Transform barrelEnd;
+
+    GameObject rocketInstance;
+
+    bool moveBullet = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (moveBullet)
+        {
+            rocketInstance = Instantiate(rocketPrefab, barrelEnd.position, barrelEnd.rotation) as GameObject;
+            moveBullet = false;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            moveBullet = true;
+
+        }
+    }
+}
---
+++ Waves/Assets/Assests/Scripts/Patrol.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed ;
+    private float  waitTime;
+    private float startWaitTime;
+    public Transform  moveSpot;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    public  float rotationSpeed;
+
+    void Start()
+    {
+        speed = 1 ;
+        rotationSpeed = 10;
+        startWaitTime = 2;
+        waitTime = startWaitTime ;
+      
+moveSpot.position = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY)) ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vector   = Vector2.MoveTowards(transform.position,moveSpot.position,speed * Time.deltaTime);
+        transform.position = vector;
+        
+        
+       // Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+
+      
+
+
+        if(Vector2.Distance(transform.position , moveSpot.position)< 0.2f){
+            if(waitTime <= 0 ){
+             moveSpot.position = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY)) ;
+                waitTime = startWaitTime;
+        } else {
+            waitTime -= Time.deltaTime;
+              Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+    }}
